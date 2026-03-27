d4ec04ec737123a3b82ac0023644b29f155e1b60
gurusg <gurugachinamath12@gmail.com>
2026-03-27 10:45:16 +0530

    text

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.mhompusvcok"
+    "revision": "0.6lgj04s6aqo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
